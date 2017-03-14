63b61296eccd897cd35f6485c55e564cdfa592a4
karen leung <karenl7@stanford.edu>
2017-03-14 16:05:09 -0700

    navigator and controller and astar files updated

--- scripts/controller.py
+++ scripts/controller.py
@@ -1,4 +1,94 @@
 #!/usr/bin/env python
 
-# FILL ME IN!
+import rospy
+from gazebo_msgs.msg import ModelStates
+from geometry_msgs.msg import Twist
+import tf
+from std_msgs.msg import Float32MultiArray
+import numpy as np
+
+class Controller:
+
+    def __init__(self):
+        rospy.init_node('turtlebot_controller', anonymous=True)
+        # rospy.Subscriber('/gazebo/model_states', ModelStates, self.callback)
+        self.trans_listener = tf.TransformListener()
+        rospy.Subscriber('/turtlebot_control/position_goal', Float32MultiArray, self.callback_desired)
+        self.pub = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size=10)
+        self.x = 0.0
+        self.y = 0.0
+        self.theta = 0.0
+        self.xg = None
+
+
+
+    def callback_desired(self, data):
+        self.xg = data.data
+
+    def get_ctrl_output(self):
+
+        try:
+          (self.position, self.orientation) = self.trans_listener.lookupTransform("/map", "/base_footprint", rospy.Time(0))
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+          self.position = (0, 0, 0)
+          self.orientation = (0, 0, 0, 1)
+
+        if self.xg is not None:
+	        self.x = self.position[0]
+	        self.y = self.position[1]  
+	        self.theta = tf.transformations.euler_from_quaternion(self.orientation)[2]  
+
+	        # use self.x self.y and self.theta to compute the right control input here
+	        # xg = np.array([1.0, 1.0])
+	        # th_g = 0.0
+	        x = np.array([self.x, self.y])
+	        rho = np.linalg.norm(x - self.xg[:2])
+
+	        alpha = np.arctan2(self.xg[1] - self.y, self.xg[0] - self.x) - self.theta
+	        delta = np.arctan2(self.xg[1] - self.y, self.xg[0] - self.x) - self.xg[2]
+
+	        if alpha < np.pi or alpha > np.pi:
+	            alpha = ( (alpha + np.pi) % (2 * np.pi)) - np.pi
+	        if delta < np.pi or delta > np.pi:
+	            delta = ( (delta + np.pi) % (2 * np.pi)) - np.pi
+
+	        k1 = 0.5
+	        k2 = 0.5
+	        k3 = 0.5
+
+	        if rho > .001:
+	            cmd_x_dot = k1 * rho * np.cos(alpha)
+	            cmd_theta_dot = k2 * alpha + k1 * (np.sinc(alpha/np.pi) * np.cos(alpha)) * (alpha + k3 * delta)
+	        else:
+	            cmd_x_dot = 0.
+	            cmd_theta_dot = k2 * (self.xg[2] - self.theta)
+
+
+	        # Apply saturation limits
+	        cmd_x_dot = np.sign(cmd_x_dot)*min(0.5, np.abs(cmd_x_dot))
+	        cmd_theta_dot = np.sign(cmd_theta_dot)*min(1, np.abs(cmd_theta_dot))
+
+	        cmd = Twist()
+	        cmd.linear.x = cmd_x_dot
+	        cmd.angular.z = cmd_theta_dot
+	        return cmd
+
+	    cmd = Twist()
+        cmd.linear.x = 0
+        cmd.angular.z = 0
+        return cmd
+
+    def run(self):
+        rate = rospy.Rate(10) # 10 Hz
+        while not rospy.is_shutdown():
+            ctrl_output = self.get_ctrl_output()
+            self.pub.publish(ctrl_output)
+            rate.sleep()
+
+if __name__ == '__main__':
+    ctrl = Controller()
+    ctrl.run()
+
+
+
 pass
--- scripts/navigator.py
+++ scripts/navigator.py
@@ -1,4 +1,123 @@
 #!/usr/bin/env python
 
-# FILL ME IN!
+from nav_msgs.msg import MapMetaData
+import numpy as np
+import matplotlib.pyplot as plt
+import tf
+from std_msgs.msg import Float32MultiArray
+from astar import AStar, StochOccupancyGrid2D, StochOccupancyGrid2D
+from nav_msgs.msg import Path
+from geometry_msgs.msg import PoseStamped
+
+class Navigator:
+
+    def __init__(self):
+        rospy.init_node('navigator', anonymous=True)
+
+        self.plan_resolution = 0.25
+        self.plan_horizon = 15
+
+        self.map_width = 0
+        self.map_height = 0
+        self.map_resolution = 0
+        self.map_origin = [0,0]
+        self.map_probs = []
+        self.occupancy = None
+
+        self.nav_sp = None
+
+        self.trans_listener = tf.TransformListener()
+
+        rospy.Subscriber("map", OccupancyGrid, self.map_callback)
+        rospy.Subscriber("map_metadata", MapMetaData, self.map_md_callback)
+        rospy.Subscriber("/turtlebot_controller/nav_goal", Float32MultiArray, self.nav_sp_callback)
+
+        self.pose_sp_pub = rospy.Publisher('/turtlebot_controller/position_goal', Float32MultiArray, queue_size=10)
+        self.nav_path_pub = rospy.Publisher('/turtlebot_controller/path_goal', Path, queue_size=10)
+        # publish status of astar
+        self.astar_status = rospy.Publisher('/turtlebot_controller/astar_status', bool, queue_size=10)
+
+    def map_md_callback(self,msg):
+        self.map_width = msg.width
+        self.map_height = msg.height
+        self.map_resolution = msg.resolution
+        self.map_origin = (msg.origin.position.x,msg.origin.position.y)
+
+    def map_callback(self,msg):
+        self.map_probs = msg.data
+        if self.map_width>0 and self.map_height>0 and len(self.map_probs)>0:
+            self.occupancy = StochOccupancyGrid2D(self.map_resolution,
+                                                  self.map_width,
+                                                  self.map_height,
+                                                  self.map_origin[0],
+                                                  self.map_origin[1],
+                                                  int(self.plan_resolution / self.map_resolution) * 2,
+                                                  self.map_probs)
+        self.send_pose_sp()   # every time the map changes, we need to update our astar path. (also updates the position goal)
+
+    def nav_sp_callback(self,msg):
+        self.nav_sp = (msg.data[0],msg.data[1],msg.data[2])
+        self.send_pose_sp()
+
+    def send_pose_sp(self):
+        try:
+            (robot_translation,robot_rotation) = self.trans_listener.lookupTransform("/map", "/base_footprint", rospy.Time(0))
+            self.has_robot_location = True
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            robot_translation = (0,0,0)
+            robot_rotation = (0,0,0,1)
+            self.has_robot_location = False
+
+        if self.occupancy and self.has_robot_location and self.nav_sp:
+            state_min = (-int(round(self.plan_horizon)), -int(round(self.plan_horizon)))
+            state_max = (int(round(self.plan_horizon)), int(round(self.plan_horizon)))
+            x_init = (int(round(robot_translation[0])), int(round(robot_translation[1])))
+            x_goal = (int(round(self.nav_sp[0])), int(round(self.nav_sp[1])))
+            astar = AStar(state_min,state_max,x_init,x_goal,self.occupancy,self.plan_resolution)
+
+            rospy.loginfo("Computing navigation plan")
+            if astar.solve():
+            	self.astar_status.publish(True)
+                
+                if len(astar.path) > 1:
+                # a naive path follower we could use
+	                pose_sp = (astar.path[1][0],astar.path[1][1],self.nav_sp[2])
+	                msg = Float32MultiArray()
+	                msg.data = pose_sp
+	                self.pose_sp_pub.publish(msg)
+	                # astar.plot_path()
+            	else:
+            		# when astar gives a single point,  position goal is tag position
+	                msg.data = self.nav_sp
+	                self.pose_sp_pub.publish(msg)
+
+
+
+                # needed for rviz
+                path_msg = Path()   # path message for rviz
+                path_msg.header.frame_id = 'map'   # path message for rviz 
+                for state in astar.path:    # for the astar solution found
+                    pose_st = PoseStamped()  # path message for rviz
+                    pose_st.pose.position.x = state[0]
+                    pose_st.pose.position.y = state[1]
+                    pose_st.header.frame_id = 'map'
+                    path_msg.poses.append(pose_st)
+                self.nav_path_pub.publish(path_msg)
+
+            else:
+            	self.astar_status.publish(False)
+                rospy.logwarn("Could not find path")
+
+    def run(self):
+    	rate = rospy.Rate(2) # 10 Hz
+    	while not rospy.is_shutdown():
+      		self.send_pose_sp()
+      		rate.sleep()
+
+
+
+if __name__ == '__main__':
+    nav = Navigator()
+    nav.run()
+
 pass
